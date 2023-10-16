ad55a4409a9f715b4493a5d677064fc70d9aeab8
kenmusembi <kenmusembi21@gmail.com>
2023-10-16 23:26:45 +0300

    ammended users api and added apis for admin, counties and  countries

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const dbqueries = require('./queries')
-
 const port = 3000
 
 app.use(bodyParser.json())
@@ -14,17 +13,39 @@ app.use(
 
 app.get('/', (request, response) => {
     response.json({
-        info: 'Node.JS, Express, and Postgres API'
+        info: 'HLove Backend API'
     })
 })
 
+//routes to users crud
 app.get('/users', dbqueries.getUsers)
 app.get('/users/:id', dbqueries.getUserById)
 app.post('/users', dbqueries.createUser)
 app.put('/users/:id', dbqueries.updateUser)
 app.delete('/users/:id', dbqueries.deleteUser)
 
+//routes to admin crud
+app.get('/admins', dbqueries.getAdmins)
+app.get('/admins/:id', dbqueries.getAdminById)
+app.post('/admins', dbqueries.createAdmin)
+app.put('/admins/:id', dbqueries.updateAdmin)
+app.delete('/admins/:id', dbqueries.deleteAdmin)
+
+//routes to county crud
+app.get('/counties', dbqueries.getCounties)
+app.get('/counties/:id', dbqueries.getCountyById)
+app.post('/counties', dbqueries.createCounty)
+app.put('/counties/:id', dbqueries.updateCounty)
+app.delete('/counties/:id', dbqueries.deleteCounty)
+
+//routes to country crud
+app.get('/countries', dbqueries.getCountries)
+app.get('/countries/:id', dbqueries.getCountryById)
+app.post('/countries', dbqueries.createCountry)
+app.put('/countries/:id', dbqueries.updateCountry)
+app.delete('/countries/:id', dbqueries.deleteCountry)
+
 app.listen(port, () =>{
-    console.log(`App running on port ${port}.`)
+    console.log(`HLove running on port ${port}.`)
 })
 
--- queries.js
+++ queries.js
@@ -24,9 +24,12 @@ const getUserById = (request, response) => {
 
 //function to post a new user to the db
 const createUser = (request, response) => {
-    const { username, email } = request.body
+    const { username, first_name, middle_name, last_name, email, phone_number, date_of_birth, description, location,
+    location_latitude, location_longitude, number_of_children, popularity} = request.body
   
-    dbconfig.pool.query('INSERT INTO users (username, email) VALUES ($1, $2)', [username, email], (error, results) => {
+    dbconfig.pool.query('INSERT INTO users (username, first_name, middle_name, last_name, email, phone_number, date_of_birth, description, location, location_latitude, location_longitude, number_of_children, popularity) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)', 
+    [username, first_name, middle_name, last_name, email, phone_number, date_of_birth, description, location,
+        location_latitude, location_longitude, number_of_children, popularity], (error, results) => {
       if (error) {
         throw error
       }
@@ -37,16 +40,18 @@ const createUser = (request, response) => {
 //function to update an existing user
 const updateUser = (request, response) => {
     const id = parseInt(request.params.id)
-    const {username, email} = request.body
+    const {username, first_name, middle_name, last_name, email, phone_number, date_of_birth, description, location,
+        location_latitude, location_longitude, number_of_children, popularity} = request.body
 
     dbconfig.pool.query(
-        'UPDATE users SET name = $1, email = $2 WHERE id = $3',
-        [username, email, id],
+        'UPDATE users SET username = $1, first_name = $2, middle_name = $3, last_name = $4, email = $5, phone_number = $6, date_of_birth = $7, description = $8, location = $9, location_latitude = $10, location_longitude = $11, number_of_children = $12, popularity = $13 WHERE id = $14',
+        [username, first_name, middle_name, last_name, email, phone_number, date_of_birth, description, location,
+            location_latitude, location_longitude, number_of_children, popularity, id],
         (error, results) => {
             if(error){
                 throw error
             }
-            response.status(200).send(`User modified with ID: ${id}`)
+            response.status(200).send(`User modified with ID: ${id}`)//returns this even if no user exists with that id
         }
     )
 }
@@ -63,10 +68,217 @@ const deleteUser = (request, response) => {
     })
 }
 
+//function to get all the admins
+const getAdmins = (request, response) => {
+    dbconfig.pool.query('SELECT * FROM admin ORDER BY id ASC', (error, results) =>{
+        if (error){
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+//function to get a single admin by ID
+const getAdminById = (request, response) => {
+    const id = parseInt(request.params.id)
+
+    dbconfig.pool.query('SELECT * FROM admin WHERE id = $1', [id], (error, results) => {
+        if (error){
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+//function to post a new admin
+const createAdmin = (request, response) => {
+    const { username, email } = request.body
+  
+    dbconfig.pool.query('INSERT INTO admin (username, email) VALUES ($1, $2)', [username, email], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).send(`Admin added with ID: ${results.insertId}`)
+    })
+  }
+
+//function to update an existing admin
+const updateAdmin = (request, response) => {
+    const id = parseInt(request.params.id)
+    const {username, email} = request.body
+
+    dbconfig.pool.query(
+        'UPDATE admin SET username = $1, email = $2 WHERE id = $3',
+        [username, email, id],
+        (error, results) => {
+            if(error){
+                throw error
+            }
+            response.status(200).send(`Admin modified with ID: ${id}`)
+        }
+    )
+}
+
+//function to delete an admin
+const deleteAdmin = (request, response) => {
+    const id = parseInt(request.params.id)
+
+    dbconfig.pool.query('DELETE FROM admin WHERE id = $1', [id], (error, results)=>{
+        if (error){
+            throw error
+        }
+        response.status(200).send(`Admin deleted with ID: ${id}`)
+    })
+}
+
+//function to get all the counties
+const getCounties = (request, response) => {
+    dbconfig.pool.query('SELECT * FROM counties ORDER BY id ASC', (error, results) =>{
+        if (error){
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+//function to get a single county by ID
+const getCountyById = (request, response) => {
+    const id = parseInt(request.params.id)
+
+    dbconfig.pool.query('SELECT * FROM counties WHERE id = $1', [id], (error, results) => {
+        if (error){
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+//function to post a new county
+const createCounty = (request, response) => {
+    const { county_code, name, active } = request.body
+  
+    dbconfig.pool.query('INSERT INTO counties (county_code, name, active) VALUES ($1, $2, $3)', [county_code, name, active], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).send(`County added with ID: ${results.insertId}`)
+    })
+  }
+
+//function to update an existing county
+const updateCounty = (request, response) => {
+    const id = parseInt(request.params.id)
+    const {county_code, name, active} = request.body
+
+    dbconfig.pool.query(
+        'UPDATE counties SET county_code = $1, name = $2, active = $3 WHERE id = $4',
+        [county_code, name, active, id],
+        (error, results) => {
+            if(error){
+                throw error
+            }
+            response.status(200).send(`County modified with ID: ${id}`)
+        }
+    )
+}
+
+//function to delete a county
+const deleteCounty = (request, response) => {
+    const id = parseInt(request.params.id)
+
+    dbconfig.pool.query('DELETE FROM counties WHERE id = $1', [id], (error, results)=>{
+        if (error){
+            throw error
+        }
+        response.status(200).send(`County deleted with ID: ${id}`)
+    })
+}
+
+//function to get all the countries
+const getCountries = (request, response) => {
+    dbconfig.pool.query('SELECT * FROM countries ORDER BY id ASC', (error, results) =>{
+        if (error){
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+//function to get a single country by ID
+const getCountryById = (request, response) => {
+    const id = parseInt(request.params.id)
+
+    dbconfig.pool.query('SELECT * FROM countries WHERE id = $1', [id], (error, results) => {
+        if (error){
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+//function to post a new country
+const createCountry = (request, response) => {
+    const { code, name, continent_name, active } = request.body
+  
+    dbconfig.pool.query('INSERT INTO countries (code, name, continent_name, active ) VALUES ($1, $2, $3, $4)', [code, name, continent_name, active ], (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(201).send(`Country added with ID: ${results.insertId}`)
+    })
+  }
+
+//function to update an existing country
+const updateCountry = (request, response) => {
+    const id = parseInt(request.params.id)
+    const {code, name, continent_name, active } = request.body
+
+    dbconfig.pool.query(
+        'UPDATE countries SET code = $1, name = $2, continent_name = $3, active = $4 WHERE id = $5',
+        [code, name, continent_name, active, id],
+        (error, results) => {
+            if(error){
+                throw error
+            }
+            response.status(200).send(`Country modified with ID: ${id}`)
+        }
+    )
+}
+
+//function to delete a country
+const deleteCountry = (request, response) => {
+    const id = parseInt(request.params.id)
+
+    dbconfig.pool.query('DELETE FROM countries WHERE id = $1', [id], (error, results)=>{
+        if (error){
+            throw error
+        }
+        response.status(200).send(`Country deleted with ID: ${id}`)
+    })
+}
+
 module.exports = {
     getUsers,
     getUserById,
     createUser,
     updateUser,
     deleteUser,
+
+    getAdmins,
+    getAdminById,
+    createAdmin,
+    updateAdmin,
+    deleteAdmin,
+
+    getCounties,
+    getCountyById,
+    createCounty,
+    updateCounty,
+    deleteCounty,
+
+    getCountries,
+    getCountryById,
+    createCountry,
+    updateCountry,
+    deleteCountry,
 }
